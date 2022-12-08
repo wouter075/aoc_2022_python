205ce5a824c3a71954068839b1e4f2100e587eed
Wouter <wouter075@gmail.com>
2022-12-08 19:35:23 +0100

    Day 8: timings == stats!

--- day8.py
+++ day8.py
@@ -1,5 +1,9 @@
+from time import time
+
 with open('day8.txt') as f:
     lines = [line.rstrip() for line in f]
+
+start_time = time()
 
 grid = []
 for line in lines:
@@ -7,6 +11,8 @@ for line in lines:
     for x in line:
         row.append(int(x))
     grid.append(row)
+
+parse_time = time()
 
 around = (len(lines) * 2) - 2 + (len(lines[0]) * 2) - 2
 part1 = around
@@ -49,6 +55,7 @@ for row in range(1, len(lines) - 1):
             part1 += 1
             continue
 
+part1_time = time()
 # part 2
 # skip outer row
 part2 = 0
@@ -92,5 +99,12 @@ for row in range(1, len(lines) - 1):
         if part2 < up * left * down * right:
             part2 = up * left * down * right
 
+part2_time = time()
+
 print(part1)
 print(part2)
+
+print("-[ stats ]" + "-" * 30)
+print("Parse time:\t", (parse_time - start_time) * 10**3, "ms")
+print("Part 1:\t\t", (part1_time - start_time) * 10**3, "ms")
+print("Part 2:\t\t", (part2_time - start_time) * 10**3, "ms")
